f01ad91dda685cbd7e7899edff72d1b7eb35eb7d
Huangsky <1174480720@qq.com>
2018-07-08 13:07:45 +0800

    更新于2018-07-08 13:05

---
+++ src/easy/TestChange.java
@@ -0,0 +1,10 @@
+package easy;
+
+/**
+ * Created by Huangsky on 2018/7/8.
+ */
+public class TestChange {
+    public static void main(String[] args){
+        System.out.println("更新一下！");
+    }
+}
