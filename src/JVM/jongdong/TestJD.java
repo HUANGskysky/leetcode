718d728ad81d4dbc1ff56f37d89b5f0a4337daa6
huangtiande <huangtiande@jd.com>
2019-10-25 23:32:32 +0800

    github test

---
+++ src/JVM/jongdong/TestJD.java
@@ -0,0 +1,21 @@
+package JVM.jongdong;
+
+/**
+ * @Description:
+ * @author:huangtiande
+ * @date:2019/10/25 23:30
+ * @Email:huangtiande@jd.com
+ */
+
+public class TestJD {
+
+    private String JD;
+
+    public String getJD() {
+        return JD;
+    }
+
+    public void setJD(String JD) {
+        this.JD = JD;
+    }
+}
